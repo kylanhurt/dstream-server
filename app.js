78b6a803399f1db08c351b2078f4ec09e0bcb9a3
kylanhurt <kylan.hurt@gmail.com>
2020-05-07 00:39:22 -0500

    Provisional completion of proper file upload

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const NodeMediaServer = require('node-media-server');
 const fs = require('fs')
 const IPFS = require('ipfs')
 const { exec } = require('child_process')
+const request = require('request')
+const axios = require('axios')
+var FormData = require('form-data');
+const concat = require('concat-stream')
+var rp = require('request-promise');
 
 const config = {
   rtmp: {
@@ -39,24 +44,58 @@ const config = {
 var nms = new NodeMediaServer(config)
 nms.run();
 
-fs.watch('./media/live/', {}, (eventType, filename) => {
+fs.watch('./media/live/', {}, async (eventType, filename) => {
   // console.log('eventType: ', eventType, ' filename: ', filename)
   if (!filename.includes('.tmp')
       && !filename.includes('.m4s')
       && !filename.includes('.m3u8')
       ) {
+        try {
+          console.log('trying filename: ', filename)
+          const url = `http://localhost:3333/upload`
+          const fileData = fs.createReadStream(`media/live/${filename}`)
+          // console.log('fileData: ', fileData)
+          var formData = new FormData()
+          formData.append('file', fileData, filename)
+          formData.append('filename', filename)
+          formData.append('kylan', 'test')
+          formData.maxDataSize = Infinity
+          const response = await rp({
+            method: 'POST',
+            url: 'http://localhost:3333/upload',
+            formData: {
+              // Like <input type="text" name="name">
+              name: 'upload',
+              // Like <input type="file" name="file">
+              file: {
+                  value: fileData,
+                  options: {
+                      filename
+                  }
+              }
+            },
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            },
+            json: true
+          })
+          console.log('response: ', response.result)
+        } catch (error) {
+          console.log('error: ', error)
+        }
+
     // need to remove to separate function that can be called
     // periodically on previously-failed uploads
     // will have to keep data in database
-    exec(`ipfs add ./media/live/${filename}`, (err, stdout, stderr) => {
-      if (err) {
-        //some err occurred
-        console.error(err)
-      } else {
-       // the *entire* stdout and stderr (buffered)
-       console.log(`stdout: ${stdout}`);
-       console.log(`stderr: ${stderr}`);
-      }
-    })
+    // exec(`ipfs add ./media/live/${filename}`, (err, stdout, stderr) => {
+    //   if (err) {
+    //     //some err occurred
+    //     console.error(err)
+    //   } else {
+    //    // the *entire* stdout and stderr (buffered)
+    //    console.log(`stdout: ${stdout}`);
+    //    console.log(`stderr: ${stderr}`);
+    //   }
+    // })
   }
 })
